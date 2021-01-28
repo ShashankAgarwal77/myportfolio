d5c1768049d305464b75aff7bfee3ac41c861297
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-01-28 14:20:04 +0530

    home page ready

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,22 @@
-import react from "react"
+import react from 'react';
+import image from '../home__img.svg';
 
 export default function Home() {
-    return <h1>Home Page!</h1>
+  return (
+    <main className='home__main flex flex-row-reverse justify-center content-center mt-8'>
+      <div>
+        <img src={image} alt='image of heatmap of website' />
+      </div>
+      <section>
+        <h1 className='text-5xl bold pr-6 mr-6'>
+          <span className=' home__main--heading-span text-green-400'>Hi,</span>{' '}
+          I'm Shashank Agarwal<span className='text-green-400'>.</span>
+        </h1>
+        <p className='text-2xl text-purple-800'>
+          Full Stack Designer and Developer
+          <span className='text-green-400'>.</span>
+        </p>
+      </section>
+    </main>
+  );
 }
