548ce4dda1d3b4b3905ae7761b02513e80226f8c
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-01-28 17:10:44 +0530

    sanity schema added and configured

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,20 @@
-import react from "react"
+import React, {useEffect, useState } from "react";
+import sanityClient from "../client.js";
 
 export default function Project() {
-    return <h1>Project Page!</h1>
+   const [projectData, setProjectData] = useState(null);
+
+   useEffect(() => {
+       sanityClient.fetch(`*[_type == "project"]{
+           title,
+           date,
+           place,
+           description,
+           projectType,
+           link,
+           tags
+       }`)
+       .then ((data) => setProjectData(data))
+       .catch(console.error);
+   }, []);
 }
