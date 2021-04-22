93cf503a356823313f0b293be03871756db070f0
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-04-22 19:23:37 +0530

    warnings removed

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import sanityClient from '../client.js';
-import bgImage from '../about_bg.jpg';
 import imageUrlBuilder from '@sanity/image-url';
 import BlockContent from '@sanity/block-content-to-react';
 
