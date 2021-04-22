1016fb2e9b2c31211317b3f73901f6471d1c18d4
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-04-22 20:06:58 +0530

    home page content added

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,18 +8,21 @@ export default function Home() {
         <img src={image} alt='avatar for profile' />
       </div>
       <section className='home__main--content'>
-        <h1 className='text-5xl bold pr-6 mr-6'>
+        <h1 className='text-3xl bold pr-6 mr-6'>
           <span className=' home__main--heading-span text-green-400'>
             {' '}
-            Hi, <div> {'\n'} </div>{' '}
+            Hello! 👋 I'm <div> {'\n'} </div>{' '}
           </span>
-          <span className=' text-white'>I'm Shashank Agarwal</span>
+          <span className=' text-white text-6xl'>Shashank Agarwal</span>
           <span className='text-green-400'>.</span>
         </h1>
         <p className='text-2xl text-white'>
           Full Stack Designer and Developer
           <span className='text-green-400'>.</span>
         </p>
+        <br></br>
+        <br></br>
+        <p className='text-xl text-white'>I am known for belief in the power of creativity, minimalism and problem solving <div> {'\n'} </div> {' '} to create a more inclusive communities<span className='text-green-400'>.</span></p>
       </section>
     </main>
   );
