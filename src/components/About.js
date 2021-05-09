075afbcecaa9df02233e28f7c4d127654b87184b
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-05-09 14:59:37 +0530

    about me styling

--- src/components/About.js
+++ src/components/About.js
@@ -33,8 +33,8 @@ export default function About() {
         alt='nature'
         className='absolute w-full'
       /> */}
-      <div className='p-10 lg:pt-48 container mx-auto relative'>
-        <section className='bg-green-800 rounded-lg shadow-2xl lg:flex p-20'>
+      <div className='p-10 lg:pt-12 container mx-auto relative'>
+        <section className='about_container bg-green-800 rounded-lg shadow-2xl lg:flex p-20'>
           <img
             src={urlFor(author.authorImage).url()}
             className='rounded w-32 h-32 lg:w-64 lg:h-64 mr-8'
