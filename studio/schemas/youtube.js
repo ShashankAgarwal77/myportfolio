4e93b36776f2534a61d6fa0c7b91cf86387a6821
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-05-28 13:46:02 +0530

    youtube preview added in sanity

--- studio/schemas/youtube.js
+++ studio/schemas/youtube.js
@@ -1,3 +1,25 @@
+import React from 'react';
+import getYouTubeID from 'get-youtube-id';
+
+const YoutubePreview = ({ value }) => {
+  const id = getYouTubeID(value.url);
+  const url = `https://www.youtube.com/embed/${id}`;
+  if (!id) {
+    return <div>Missing Youtube URL</div>;
+  }
+  return (
+    <iframe
+      width='560'
+      height='315'
+      src={url}
+      title='YouTube Video Player'
+      frameborder='0'
+      allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+      allowfullscreen
+    ></iframe>
+  );
+};
+
 export default {
   name: 'youtube',
   type: 'object',
@@ -9,4 +31,11 @@ export default {
       title: 'URL',
     },
   ],
+
+  preview: {
+    select: {
+      url: 'url',
+    },
+    component: YoutubePreview,
+  },
 };
