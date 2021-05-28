9f9317712885b3d3d2137d391e2cf10f6fa7d07f
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-05-28 15:28:25 +0530

    shit commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import SinglePost from './components/SinglePost';
