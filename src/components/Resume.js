5cdae59749428d25dc4cf65edaa434bd038e0ea3
Shashank Agarwal <shishya.agarwal@gmail.com>
2022-06-16 14:55:17 +0530

    resume updated

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,12 +3,12 @@ import React, { useEffect } from "react";
 function Resume() {
   useEffect(() => {
     window.location.href =
-      "https://drive.google.com/file/d/1rRqH16Y1Aici7lqysiNlP-DgRfHvh2Qz/view?usp=sharing";
+      "https://drive.google.com/file/d/1TlRx7VCF1J0osoCSQ6eU_LAjYq_9Sv0i/view?usp=sharing";
   }, []);
 
   return (
     <a
-      href="https://drive.google.com/file/d/1rRqH16Y1Aici7lqysiNlP-DgRfHvh2Qz/view?usp=sharing"
+      href="https://drive.google.com/file/d/1TlRx7VCF1J0osoCSQ6eU_LAjYq_9Sv0i/view?usp=sharing"
       alt="product design resume"
     >
       Resume
