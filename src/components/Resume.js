4dd5e7d0e9f8003983c80277a983f956650fda9d
Shashank Agarwal <shashankagarwal.official@gmail.com>
2023-06-26 23:57:41 +0530

    Update Resume.js
    resume updated - timeline in cogoport fixed

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,12 +3,12 @@ import React, { useEffect } from "react";
 function Resume() {
   useEffect(() => {
     window.location.href =
-      "https://drive.google.com/file/d/1TlRx7VCF1J0osoCSQ6eU_LAjYq_9Sv0i/view?usp=sharing";
+      "https://drive.google.com/file/d/1frtizzIWYHBqi8jshXQjsXQ07R-8xBe5/view?usp=sharing";
   }, []);
 
   return (
     <a
-      href="https://drive.google.com/file/d/1TlRx7VCF1J0osoCSQ6eU_LAjYq_9Sv0i/view?usp=sharing"
+      href="https://drive.google.com/file/d/1frtizzIWYHBqi8jshXQjsXQ07R-8xBe5/view?usp=sharing"
       alt="product design resume"
     >
       Resume
