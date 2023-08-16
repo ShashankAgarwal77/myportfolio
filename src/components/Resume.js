776a7426ecb2ab5052309c5e9c1e95841e8399bf
ShashankAgarwal77 <shashankagarwal.official@gmail.com>
2023-08-16 15:30:12 +0530

    new push

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,12 +3,12 @@ import React, { useEffect } from "react";
 function Resume() {
   useEffect(() => {
     window.location.href =
-      "https://drive.google.com/file/d/1frtizzIWYHBqi8jshXQjsXQ07R-8xBe5/view?usp=sharing";
+      "https://drive.google.com/file/d/1KH3zQpkg5jToo0Svd0Nu3tR6O81SMrYX/view?usp=sharing";
   }, []);
 
   return (
     <a
-      href="https://drive.google.com/file/d/1frtizzIWYHBqi8jshXQjsXQ07R-8xBe5/view?usp=sharing"
+      href="https://drive.google.com/file/d/1KH3zQpkg5jToo0Svd0Nu3tR6O81SMrYX/view?usp=sharing"
       alt="product design resume"
     >
       Resume
